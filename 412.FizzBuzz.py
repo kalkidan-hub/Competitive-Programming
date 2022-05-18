3aaa9447a9f877ad9fc936b7e314f85f6cfc530a
kalkidan-hub <kalkidandereje666@gmail.com>
2022-05-18 22:57:50 +0300

    EASY

---
+++ 412.FizzBuzz.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        s=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                s+=["FizzBuzz"]
+            elif i%3==0:
+                s+=["Fizz"]
+            elif i%5==0:
+                s+=["Buzz"]
+            else:
+                s+=[str(i)]
+        return s
