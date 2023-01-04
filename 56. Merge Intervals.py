7d16221da1de667f90f946ea34ac701d5e603c68
kalkidan-hub <kalkidandereje666@gmail.com>
2023-01-05 02:57:12 +0300

    Medium

--- 56. Merge Intervals.py
+++ 56. Merge Intervals.py
@@ -1,17 +1,26 @@
 def merge(intervals):
+    intervals.sort()
     merged=[]
+    ct=0
     for i in range(len(intervals)):
+        if ct>0:
+            ct-=1
+            continue
         subInterval=intervals[i]
         for j in range(i+1,len(intervals)):
-            if subInterval[1]>=intervals[j][0]:
+            if subInterval[1]>=intervals[j][0]and subInterval[1]<=intervals[j][1] :
                 subInterval[1]=intervals[j][1]
+                ct+=1
+            elif subInterval[1]>=intervals[j][0] and subInterval[1]>=intervals[j][1]:
+                ct+=1
             else: break
-    merged+=[subInterval]
+        merged.append(subInterval)
+        
+        
 
     return merged
 
 
 
 
-
-print(merge([[1,3],[2,3],[3,6],[8,10],[15,18]]))
+print(merge([[1,3],[2,6],[8,10],[15,18]]))
