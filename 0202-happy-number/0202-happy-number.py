2dfb167ab012713c82910537de5f75663c4e0635
kal <93663038+kalkidan-hub@users.noreply.github.com>
2023-10-20 10:39:02 +0300

    Time: 33 ms (95.32%), Space: 16.1 MB (78.83%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen_before = set()
+        
+        while True:
+            
+            if n in seen_before:
+                break
+            elif n == 1:
+                return True
+            
+            seen_before.add(n)
+            new_n = 0
+            for i in str(n):
+                new_n += int(i) ** 2
+            n = new_n
+        # print(2**31 -1)
+        return False
