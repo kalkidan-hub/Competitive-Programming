79636db092a7d9342203e9b78e44e3a911df21dc
kal <93663038+kalkidan-hub@users.noreply.github.com>
2023-10-25 06:40:59 +0300

    Time: 139 ms (26.56%), Space: 16.1 MB (89.23%) - LeetHub

--- 0679-24-game/0679-24-game.py
+++ 0679-24-game/0679-24-game.py
@@ -1,64 +1,8 @@
 class Solution:
     def judgePoint24(self, cards: List[int]) -> bool:
-        if cards == [3,3,8,8]:
-            return True
-        
-        cards = list(permutations(cards))
-        operations = ['+', '-', '*', '/']
-        is_there = False
-
-        def parenthesizing(exp):
-            if len(exp) == 1:
-                    return [int(exp[0])]
-
-            res = [eval("".join(exp))]
-            for i in range(1,len(exp),2):
-                left = exp[:i]
-                operation = exp[i]
-                right = exp[i + 1:]
-
-                left_res = parenthesizing(left)
-                right_res = parenthesizing(right)
-
-                for k in left_res:
-                    for j in right_res:
-                        if operation == '+':
-                             res.append(k + j)
-                        elif operation == '-':
-                             res.append(k - j)
-                        elif operation == '*':
-                             res.append(k * j)
-                        elif operation == '/':
-                             if j != 0:
-                                res.append(k / j)
-            
-            return res
-                   
-        for card in cards:
-            
-            exp = [str(card[0])]
-
-            for i in operations:
-                exp.append(i)
-                exp.append(str(card[1]))
-                for j in operations:
-                    exp.append(j)
-                    exp.append( str(card[2]))
-                    for k in operations:
-                        exp.append(k)
-                        exp.append(str(card[3]))
-
-                        val = parenthesizing(exp)
-                        if 24 in val or 24.000 in val:
-                             is_there = True
-
-                        if is_there:
-                            return True
-
-                        exp.pop()
-                        exp.pop()
-                    exp.pop()
-                    exp.pop()
-                exp.pop()
-                exp.pop()
-     
+        if len(cards) == 1:
+            return math.isclose(cards[0],24)
+    
+        return any(self.judgePoint24([x] + rest)
+                    for a,b,*rest in itertools.permutations(cards)
+                    for x in {a+b,a-b,a*b,b and a/b})
